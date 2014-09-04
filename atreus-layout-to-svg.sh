9f269760a6f0ea4c0feeb4c3a3b67703ac74a7e3
Aaron Ash <aaron.ash@gmail.com>
2014-09-04 20:11:30 +0800

    Added in key renderings

--- atreus-layout-to-svg.sh
+++ atreus-layout-to-svg.sh
@@ -12,6 +12,54 @@ if [ $argc -ne 1 ]; then
   exit
 fi 
 
+function rendercharacters {
+  # These replacements must come in a certain order or they clobber each other.
+  echo $1 | sed \
+  -e 's/"//g' \
+  -e 's/\[//g' \
+  -e 's/\]//g' \
+  -e 's/,//g' \
+  -e 's/shift1/!/' \
+  -e 's/shift2/@/' \
+  -e 's/shift3/#/' \
+  -e 's/shift4/\$/' \
+  -e 's/shift5/%/' \
+  -e 's/shift6/\^/' \
+  -e 's/shift7/&/' \
+  -e 's/shift8/*/' \
+  -e 's/shift9/(/' \
+  -e 's/shift0/)/' \
+  -e 's/shiftTILDE/~/' \
+  -e 's/shiftMINUS/_/' \
+  -e 's/shiftEQUALS/+/' \
+  -e 's/shiftBACKSLASH/|/' \
+  -e 's/shiftLEFT_BRACE/{/' \
+  -e 's/shiftRIGHT_BRACE/}/' \
+  -e 's/PAGE_DOWN/PgDn/' \
+  -e 's/PAGE_UP/PgUp/' \
+  -e 's/BACKSPACE/← BS/' \
+  -e 's/COMMA/,/' \
+  -e 's/DELETE/Del/' \
+  -e 's/DOWN/↓/' \
+  -e 's/ENTER/⏎/' \
+  -e 's/EQUAL/=/' \
+  -e 's/FN/fn/' \
+  -e 's/INSERT/Ins/' \
+  -e 's/LEFT_BRACE/[/' \
+  -e 's/LEFT/←/' \
+  -e 's/MINUS/-/' \
+  -e 's/PERIOD/./' \
+  -e "s/QUOTE/'/" \
+  -e 's/RIGHT_BRACE/]/' \
+  -e 's/RIGHT/→/' \
+  -e 's/SEMICOLON/;/' \
+  -e 's/TILDE/`/' \
+  -e 's/UP/↑/' \
+  -e 's/function/Layer/' \
+  -e 's/BACKSLASH/\\/' \
+  -e 's/SLASH/\\\//'
+}
+
 layoutfile=$1
 layercount=$(./remccoms3.sed $layoutfile | jq length )
 htmlfile="$layoutfile.html"
@@ -32,9 +80,14 @@ do
   for j in {0..41}
   do
     key=$(echo "$layer" | jq -c -r .["$j"])
+    key=$(rendercharacters "$key")
     echo "key $j = $key"
     sed -i "" "s/>$j</>$key</" "$layerfile"
   done
   cat "$layerfile" >> "$htmlfile"
   rm "$layerfile"
 done
+
+
+
+
