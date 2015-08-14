cbbb4c832a9fb72b8c8d8ba4654c257f009f4e01
James Reid-Smith <james.reidsmith@shopify.com>
2015-08-14 10:06:13 -0400

    Fixed issue with number replacement in svg, if layout contained numbers in the begining of the layout later key replacements would also replace them. Fixed shiftEQUAL replacement in svg.

--- atreus-layout-to-svg.sh
+++ atreus-layout-to-svg.sh
@@ -42,7 +42,7 @@ function rendercharacters {
   -e 's/shift0/)/' \
   -e 's/shiftTILDE/~/' \
   -e 's/shiftMINUS/_/' \
-  -e 's/shiftEQUALS/+/' \
+  -e 's/shiftEQUAL/+/' \
   -e 's/shiftBACKSLASH/|/' \
   -e 's/shiftLEFT_BRACE/{/' \
   -e 's/shiftRIGHT_BRACE/}/' \
@@ -109,12 +109,9 @@ do
       #cat "$layerfile" | sed -e "s/>$((j + 1))</>$key</" > "$layerfile"
       sed -i -e "s/>$((j + 1))</>$key</" "$layerfile"
     elif [[ "$platform" == 'Darwin' ]]; then
-      sed -i "" -e "s/>$((j + 1))</>$key</" "$layerfile"
+      sed -i "" -e "s/>x$((j + 1))x</>$key</" "$layerfile"
     fi
   done
   cat "$layerfile" >> "$htmlfile"
   rm "$layerfile"
 done
-
-
-
