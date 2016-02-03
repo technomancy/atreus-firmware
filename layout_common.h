69dcf4eb477f758ed0113cef87543baf5bfcd721
Chris Gerber <christopher.gerber@regeneron.com>
2016-02-02 22:20:55 -0500

    Try out a fully "Lorent" world on the Atreus
    This change does a few things:
    * Make fn+Shift momentarily switch to layer 2
    * Move reset to the N on layer 2
    * Make the old reset and Enter again on layer 2
    * Swap the arrows for the FN keys on layer 2 (left/right hand)
    * To use shift on layer 2 you must jump, rather than use momentary

--- layout_common.h
+++ layout_common.h
@@ -4,14 +4,20 @@ void activate_fn() {
   fn_decay = 20;
 };
 
+int layer_to_momentary = 1;
+
+void layer_momentary() {
+  layer_to_momentary = 2;
+};
+
 int layer_to_jump = 0;
 
 // jump to this layer when fn is released
 void layer_jump() {
-  layer_to_jump = 2;
+  layer_to_jump = 3;
 };
 
-void (*layer_functions[])(void) = {reset, activate_fn, layer_jump};
+void (*layer_functions[])(void) = {reset, activate_fn, layer_jump, layer_momentary};
 
 // When we are sending key combinations that include modifiers, the OS
 // can do some level of error-correction to prevent this scenario:
@@ -30,12 +36,13 @@ void (*layer_functions[])(void) = {reset, activate_fn, layer_jump};
 
 void per_cycle() {
   if(fn_decay > 1) {
-    current_layer = layers[1];
+    current_layer = layers[layer_to_momentary];
     fn_decay--;
   } else if(fn_decay == 1) {
     current_layer_number = layer_to_jump;
     fn_decay--;
   } else {
+    layer_to_momentary = 1;
     layer_to_jump = 0;
     fn_decay = 0;
   }
